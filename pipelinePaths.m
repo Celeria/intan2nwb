cecbcff956267d44f319174895831a3b1b28ae30
Patrick <Patrick@10.32.133.208>
2023-04-05 15:59:39 -0500

    changed the path of the scratch folder to be correct

--- pipelinePaths.m
+++ pipelinePaths.m
@@ -17,7 +17,7 @@ pp.CONDA        = 'C:\Users\westerja.VUDS\Anaconda3';
 pp.REPO         = 'C:\Users\westerja.VUDS\Documents\PreProcessorv2\intan2nwb\';
 pp.TBOXES       = 'C:\Users\westerja.VUDS\Documents\PreProcessorV2\';
 
-pp.SCRATCH      = 'D:\scratch';
+pp.SCRATCH      = 'D:\_BL_DATA_PIPELINE\scratch';
 
 % Varargin
 varStrInd = find(cellfun(@ischar,varargin));                        
