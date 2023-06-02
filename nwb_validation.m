8f882b2c1241be96882193e5a0ce26810366435b
Patrick <Patrick@10.32.133.208>
2023-06-02 13:35:28 -0500

    modified with Hamed's better counting code

--- nwb_validation.m
+++ nwb_validation.m
@@ -24,8 +24,6 @@ cd(pp.NWB_DATA);
 nwb_file_list = dir('*.nwb');
 cd(old_dir);
 
-
-
 for ii = 1:length(nwb_file_list)
     path_to_nwb = string(nwb_file_list(ii).folder) + "\";
     nwb_name = string(nwb_file_list(ii).name);
@@ -36,33 +34,40 @@ for ii = 1:length(nwb_file_list)
 
     %Print out passive glo specific information
     try
-        passive_glo_info = nwb.intervals.get('passive_glo');
+        %Written by Hamed Nejat, to count the number of trials in passive
+        %glo for each block, and trial types. Modified by Patrick Meng to
+        %append to the slack notification text
         a = (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 1) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);
         b = (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 2) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);
-        c = (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 3) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);
+        c = (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 3) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);    
         d = (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 4) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);
-
-        slack_text = slack_text + string("\n-> hab(1) = %d, Main(2) = %d, rndctl(3) = %d, seqctl(4) = %d ", sum(a), sum(b), sum(c), sum(d));
-        x = sum(a);
+        
+        slack_text = slack_text + sprintf("> Session %s\n\n->Total correct trials: %d\n", nwb.identifier, sum(a + b + c + d));
+        slack_text = slack_text + sprintf("-> Habituation(1) = %d, Main(2) = %d, Random control(3) = %d, Sequence control(4) = %d \n", sum(a), sum(b), sum(c), sum(d));
+        slack_text = slack_text + sprintf("\n--> (1) LO habituation : %d \n", sum(a));
+    
         a = nwb.intervals.get("passive_glo").vectordata.get("go_gloexp").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 2);
-        slack_text = slack_text + string("\n-> GO main : %d ", sum(a));
+        slack_text = slack_text + sprintf("\n--> (2) GO main : %d ", sum(a));
         b = nwb.intervals.get("passive_glo").vectordata.get("lo_gloexp").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("task_block_number").data(:) == 2);
-        slack_text = slack_text + string("\n-> LO main : %d ", sum(b));
-
-        a = nwb.intervals.get("passive_glo").vectordata.get("go_rndctl").data(:);% & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> GO rnd control : %d ", sum(a));
-        b = nwb.intervals.get("passive_glo").vectordata.get("lo_rndctl").data(:);% & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> LO rnd control : %d ", sum(b));
-
-        c = nwb.intervals.get("passive_glo").vectordata.get("igo_rndctl").data(:);% & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> iGO rnd control : %d ", sum(c));
-        d = nwb.intervals.get("passive_glo").vectordata.get("ilo_rndctl").data(:);% & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> iLO rnd control : %d ", sum(d));
-
+        slack_text = slack_text + sprintf("\n--> (2) LO main : %d \n", sum(b));
+    
+        a = nwb.intervals.get("passive_glo").vectordata.get("go_rndctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
+        slack_text = slack_text + sprintf("\n--> (3) GO random control : %d ", sum(a));
+        b = nwb.intervals.get("passive_glo").vectordata.get("lo_rndctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
+        slack_text = slack_text + sprintf("\n--> (3) LO random control : %d ", sum(b));
+        
+        c = nwb.intervals.get("passive_glo").vectordata.get("igo_rndctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
+        slack_text = slack_text + sprintf("\n--> (3) iGO random control : %d ", sum(c));
+        d = nwb.intervals.get("passive_glo").vectordata.get("ilo_rndctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
+        slack_text = slack_text + sprintf("\n--> (3) iLO random control : %d ", sum(d));
+    
+        e = nwb.intervals.get("passive_glo").vectordata.get("rndctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:) & (nwb.intervals.get("passive_glo").vectordata.get("stimulus_number").data(:) == 5);
+        slack_text = slack_text + sprintf("\n--> (3) Other random control : %d \n", sum(e) - sum(a + b + c + d));
+    
         a = nwb.intervals.get("passive_glo").vectordata.get("go_seqctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> GO seq control : %d ", sum(a));
+        slack_text = slack_text + sprintf("\n--> (4) GO sequence control : %d ", sum(a));
         b = nwb.intervals.get("passive_glo").vectordata.get("igo_seqctl").data(:) & nwb.intervals.get("passive_glo").vectordata.get("correct").data(:);
-        slack_text = slack_text + string("\n-> iGO seq control : %d ", sum(b));
+        slack_text = slack_text + sprintf("\n--> (4) iGO sequence control : %d \n", sum(b));
     catch
     end
 
