42d00504778cbbf271bba6295a1a554f02fea9d4
Patrick <patm314@gmail.com>
2023-07-08 01:17:32 -0500

    did a memory map which is supposedly faster

--- process_binary_data.py
+++ process_binary_data.py
@@ -7,14 +7,14 @@ Created on Wed Jun  7 13:46:32 2023
 
 def process_binary_files(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name):
 
-    import numpy as np
     import os
+    import numpy as np
 
-    data_to_write = np.zeros((NUM_CHANNELS, num_samples), dtype=np.int16)
-
+    # Create a memory-mapped array with the same dtype and shape as data_to_write
+    fp = np.memmap(file_name, dtype='int16', mode='w+', shape=(num_samples, NUM_CHANNELS))
+    
     for ii in range(NUM_CHANNELS):
         with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_file:
-            data_to_write[ii, :] = np.fromfile(current_file, dtype=np.int16, count=num_samples)
+            fp[:, ii] = np.fromfile(current_file, dtype=np.int16, count=num_samples)
 
-    # Transpose the data to match MATLAB's column-major order
-    data_to_write.T.tofile(file_name)
+    del fp  # this line ensures any remaining changes are written to disk before removing the object
