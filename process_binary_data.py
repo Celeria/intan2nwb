89df196fcbbba051965c8b009e217188596401e7
Patrick <patm314@gmail.com>
2023-07-07 23:59:20 -0500

    another version to see if it'll get it this time

--- process_binary_data.py
+++ process_binary_data.py
@@ -5,16 +5,19 @@ Created on Wed Jun  7 13:46:32 2023
 @author: Patrick, translated from MATLAB by chatgpt-4
 """
 
-def process_binary_data(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name):
+def process_binary_files(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name):
     import numpy as np
+    from concurrent.futures import ProcessPoolExecutor
     import os
 
     data_to_write = np.zeros((NUM_CHANNELS, num_samples), dtype=np.int16)
 
-    for ii in range(NUM_CHANNELS):
-        with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_fid:
-            data_to_write[ii, :] = np.fromfile(current_fid, dtype=np.int16, count=num_samples)
-            
-    with open(file_name, 'wb') as writtenFileID:
-        data_to_write = np.reshape(data_to_write, data_to_write.shape, order='F')
-        writtenFileID.write(data_to_write.tobytes())
+    def read_data(ii):
+        with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_file:
+            return np.fromfile(current_file, dtype=np.int16, count=num_samples)
+
+    with ProcessPoolExecutor() as executor:
+        for ii, data in enumerate(executor.map(read_data, range(NUM_CHANNELS)), 1):
+            data_to_write[ii-1, :] = data
+
+    data_to_write.tofile(file_name)
