e7da74aac9364ea197d6d45d5a0699c18670f9b1
Patrick <patm314@gmail.com>
2023-07-07 22:56:57 -0500

    stuck with the old ways

--- process_binary_data.py
+++ process_binary_data.py
@@ -5,12 +5,14 @@ Created on Wed Jun  7 13:46:32 2023
 @author: Patrick, translated from MATLAB by chatgpt-4
 """
 
-def process_binary_data(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name, chunk_size=10000000):
+def process_binary_data(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name):
     import numpy as np
     import os
+    data_to_write = np.zeros((NUM_CHANNELS, num_samples), dtype=np.int16)
+
+    for ii in range(NUM_CHANNELS):
+        with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_fid:
+            data_to_write[ii, :] = np.fromfile(current_fid, dtype=np.int16, count=num_samples)
+            
     with open(file_name, 'wb') as writtenFileID:
-        for ii in range(NUM_CHANNELS):
-            with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_fid:
-                for i in range(0, num_samples, chunk_size):
-                    data_to_write = np.fromfile(current_fid, dtype=np.int16, count=min(chunk_size, num_samples - i))
-                    data_to_write.tofile(writtenFileID)
+        data_to_write.tofile(writtenFileID)
