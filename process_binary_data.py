db41e1ea010a89f7d96dea603354d3097f9d8eba
Patrick <patm314@gmail.com>
2023-07-08 00:29:01 -0500

    srsly

--- process_binary_data.py
+++ process_binary_data.py
@@ -8,18 +8,13 @@ Created on Wed Jun  7 13:46:32 2023
 def process_binary_files(NUM_CHANNELS, num_samples, in_file_path, port_letter, file_name):
 
     import numpy as np
-    from concurrent.futures import ProcessPoolExecutor
     import os
 
     data_to_write = np.zeros((NUM_CHANNELS, num_samples), dtype=np.int16)
 
-    def read_data(ii):
+    for ii in range(NUM_CHANNELS):
         with open(os.path.join(in_file_path, f"amp-{port_letter.upper()}-{ii:03}.dat"), 'rb') as current_file:
-            return np.fromfile(current_file, dtype=np.int16, count=num_samples)
-
-    with ProcessPoolExecutor() as executor:
-        for ii, data in enumerate(executor.map(read_data, range(NUM_CHANNELS)), 1):
-            data_to_write[ii-1, :] = data
+            data_to_write[ii, :] = np.fromfile(current_file, dtype=np.int16, count=num_samples)
 
     # Transpose the data to match MATLAB's column-major order
     data_to_write.T.tofile(file_name)
