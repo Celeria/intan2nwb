f595850a36f30cfa3b89fdc3fe55444e585cab22
Patrick <patm314@gmail.com>
2023-07-10 20:47:11 -0500

    changed it so it doesn't crash matlab, hopefully

--- mergeBinaryFiles.cpp
+++ mergeBinaryFiles.cpp
@@ -21,7 +21,11 @@ void mexFunction(int nlhs, mxArray *plhs[],
     std::string file_name(mxArrayToString(prhs[4]));
 
     // Container to hold all data
-    std::vector<std::vector<int16_t>> data_to_write(num_channels, std::vector<int16_t>(num_samples, 0));
+    try {
+        std::vector<std::vector<int16_t>> data_to_write(num_channels, std::vector<int16_t>(num_samples, 0));
+    } catch (std::bad_alloc& ba) {
+        mexErrMsgTxt("Memory allocation failed.");
+    }
     std::ifstream current_fid;
     std::string in_file_name;
 
@@ -29,16 +33,42 @@ void mexFunction(int nlhs, mxArray *plhs[],
         in_file_name = in_file_path + "amp-" + std::string(1, toupper(port_letter[0])) + "-" + std::to_string(ii-1) + ".dat";
         current_fid.open(in_file_name, std::ios::binary);
 
+        // Check if file opened successfully
+        if(!current_fid) {
+            mexErrMsgIdAndTxt("MATLAB:fileOpenError",
+                              ("Unable to open file: " + in_file_name).c_str());
+        }
+
         // Read binary data
         current_fid.read((char*)&data_to_write[ii - 1][0], num_samples * sizeof(int16_t));
+        
+        // Check if reading was successful
+        if(!current_fid) {
+            mexErrMsgIdAndTxt("MATLAB:fileReadError",
+                              ("Error reading file: " + in_file_name).c_str());
+        }
+
         current_fid.close();
     }
 
     // Transpose and write to binary file
     std::ofstream written_fid(file_name, std::ios::binary);
+
+    // Check if file opened successfully
+    if(!written_fid) {
+        mexErrMsgIdAndTxt("MATLAB:fileOpenError",
+                          ("Unable to open file: " + file_name).c_str());
+    }
+
     for(int jj = 0; jj < num_samples; jj++) {
         for(int ii = 0; ii < num_channels; ii++) {
             written_fid.write((char*)&data_to_write[ii][jj], sizeof(int16_t)); // Note the transpose operation here
+
+            // Check if writing was successful
+            if(!written_fid) {
+                mexErrMsgIdAndTxt("MATLAB:fileWriteError",
+                                  ("Error writing file: " + file_name).c_str());
+            }
         }
     }
     written_fid.close();
